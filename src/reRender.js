d92dc0078f0ca143b00564265b540e38b33f151b
AndMkh7 <andmkhart77@gmail.com>
2022-02-02 20:35:36 +0400

    Making a functional addition of a post. Create a rerender function for rendering the page after the change.

--- src/reRender.js
+++ src/reRender.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {addPost} from "./redux/state";
-
+import state from "./redux/state";
+ 
 let reRender= (state) =>{
 
     ReactDOM.render(
