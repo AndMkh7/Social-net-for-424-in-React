59ece57522693cfd44259764aabc7adb093eb0a3
AndMkh7 <andmkhart77@gmail.com>
2022-03-30 17:05:32 +0400

    Refactoring Functional Component Users.jsx to Classical Component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,64 +3,41 @@ import styles from "./users.module.css";
 import * as axios from "axios";
 import userPhoto from "../../../src/assets/images/user.png"
 
-let Users = (props) => {
-    if (props.users.length === 0){/*
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: "http://www.footballtop.ru/sites/default/files/styles/player_full/public/photos/coaches/carlo-ancelotti.jpg?itok=JN6vfpFo",
-                followed: false,
-                fullName: "Anchelotti",
-                status: "I am a coach",
-                location: {country: "Italy", city: "Milano"}
-            },
-            {
-                id: 2,
-                photoUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Liverpool_vs._Chelsea%2C_UEFA_Super_Cup_2019-08-14_04.jpg/342px-Liverpool_vs._Chelsea%2C_UEFA_Super_Cup_2019-08-14_04.jpg",
-                followed: true,
-                fullName: "Klop",
-                status: "I am a coach assistant",
-                location: {country: "Germany", city: "Dortmund"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://i2-prod.manchestereveningnews.co.uk/sport/football/football-news/article23220379.ece/ALTERNATES/s810/0_GettyImages-1238589476.jpg",
-                followed: false,
-                fullName: "Pep",
-                status: "I am a coach assistant too",
-                location: {country: "Spain", city: "Barcelona"}
-            },
-        ])*/
+class Users extends React.Component {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").
-        then(response=>{
-           props.setUsers(response.data.items)
+    constructor(props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
         });
     }
 
+    render() {
+        return <div>
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+            {
+                this.props.users.map(u => <div key={u.id}>
                      <span>
                         <div>
-                            <img src={u.photos.small!=null ? u.photos.small:userPhoto} className={styles.userPhoto} alt={""}/>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
+                                 alt={""}/>
                         </div>
                          <div>
 
 
                              {u.followed ?
                                  <button onClick={() => {
-                                     props.unfollow(u.id)
+                                     this.props.unfollow(u.id)
                                  }}>UnFollow</button> :
                                  <button onClick={() => {
-                                     props.follow(u.id)
+                                     this.props.follow(u.id)
                                  }}>Follow</button>}
 
 
                          </div>
                      </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -70,10 +47,12 @@ let Users = (props) => {
                             <div>{"u.location.city"}</div>
                         </span>
                      </span>
-                </div>
-            )
-        }
-    </div>
+                    </div>
+                )
+            }
+        </div>
+
+    }
 
 }
 
